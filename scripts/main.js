8de0dc1f89fb4dc68078fececd8782ad40efaf8f
tsktsk <office@cateringbyandrew.com>
2020-02-17 14:42:55 -0500

    kvh logo switch

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+let logos = document.querySelector('img');
+logos.onclick = function()
+{
+  let logoSrc = logos.getAttribute('src');
+  if (logoSrc === 'images/cba-logo.png')
+  {
+    logos.setAttribute('src', 'images/kvh-logo.png');
+  }
+  else
+  {
+    logos.setAttribute('src', 'images/cba-logo.png');
+  }
+}

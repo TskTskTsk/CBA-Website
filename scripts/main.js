fa56ebdc8866e40818d18bc63f18d58798eab4c1
tsktsk <office@cateringbyandrew.com>
2020-02-18 16:01:19 -0500

    Added exception handling and some bug fixing. Fully functional MDN intro content. All features 100%

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,35 @@
-
 //Logo Switcher
-
-
 let logos = document.querySelector('img');
-logos.onclick = function()
-{
+logos.onclick = function() {
   let logoSrc = logos.getAttribute('src');
-  if (logoSrc === 'images/cba-logo.png')
-  {
+  if (logoSrc === 'images/cba-logo.png') {
     logos.setAttribute('src', 'images/kvh-logo.png');
-  }
-  else
-  {
+  } else {
     logos.setAttribute('src', 'images/cba-logo.png');
   }
 }
-
-
-
-
-
-//Custom Greeting
-
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 
-function setUserName()
-{
+function setUserName() {
   let myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName)
-  myHeading.textContent = 'Greetings, ' + myName;
+  if (!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Greetings, ' + myName;
+  }
 }
 
-if(!localStorage.getItem('name'))
-{
+if(!localStorage.getItem('name')) {
   setUserName();
-}
-else
-{
+} else {
   let storedName = localStorage.getItem('name');
-  myHeading.textConten = 'Greetings, ' + storedName;
+  myHeading.innerHTML = 'Greetings, ' + storedName;
 }
 
-myButton.onclick = function()
-{
+myButton.onclick = function() {
   setUserName();
 }
